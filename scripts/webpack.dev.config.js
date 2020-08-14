0f7662fa0aa2d5ade9311efcea11731223958fae
dbwjay <dbwjay@126.com>
2020-08-14 18:01:49 +0800

    feat(build): back up

--- scripts/webpack.dev.config.js
+++ scripts/webpack.dev.config.js
@@ -1,33 +1,33 @@
-const merge = require('webpack-merge')
-const path = require('path')
-const os = require('os')
+const merge = require('webpack-merge');
+const path = require('path');
+const os = require('os');
 
-const baseConfig = require('./webpack.base.config')
-const proxyConfig = require('./config/proxy')
+const baseConfig = require('./webpack.base.config');
+const proxyConfig = require('./config/proxy');
 
 /**
  * 获取本机的ip地址
  * @returns {string}
  */
 const getIPAddress = () => {
-  let interfaces = os.networkInterfaces()
+  let interfaces = os.networkInterfaces();
   for (let devName in interfaces) {
-    let iface = interfaces[devName]
+    let iface = interfaces[devName];
     for (let i = 0; i < iface.length; i++) {
-      let alias = iface[i]
+      let alias = iface[i];
       if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-        return alias.address
+        return alias.address;
       }
     }
   }
-}
-const myHost = getIPAddress()
+};
+const myHost = getIPAddress();
 
 module.exports = merge(baseConfig, {
   mode: 'development',
   devtool: 'source-map', // 调试源码
   output: {
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -46,10 +46,10 @@ module.exports = merge(baseConfig, {
           'vue-style-loader',
           'css-loader',
           'less-loader',
-          'postcss-loader'
-        ]
+          'postcss-loader',
+        ],
       },
-    ]
+    ],
   },
   devServer: {
     contentBase: path.join(__dirname, '../dist/'),
@@ -59,6 +59,6 @@ module.exports = merge(baseConfig, {
     hot: true,
     overlay: true,
     proxy: proxyConfig,
-    historyApiFallback: true
-  }
-})
+    historyApiFallback: true,
+  },
+});
