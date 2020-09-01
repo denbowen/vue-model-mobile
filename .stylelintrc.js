88a0cf86361486fb5d1ae0ce0aea45f12dc7872c
dbwjay <dbwjay@126.com>
2020-09-01 10:59:59 +0800

    feat: fulfill lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-    extends: [
-        "stylelint-config-standard",
-        "stylelint-config-recess-order",
-        "stylelint-config-prettier"
-    ],
-    rules: {
-        "no-empty-source": null,
-        "no-descending-specificity": null,
-        "at-rule-no-unknown": [
-            true,
-            {
-                "ignoreAtRules": [
-                    "mixin",
-                    "extend",
-                    "content"
-                ]
-            }
-        ],
-        "selector-pseudo-class-no-unknown": [
-            true,
-            {
-                "ignorePseudoClasses": [
-                    "global"
-                ]
-            }
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+    "stylelint-config-prettier"
+  ],
+  rules: {
+    "no-empty-source": null,
+    "no-descending-specificity": null,
+    "at-rule-no-unknown": [
+      true,
+      {
+        "ignoreAtRules": [
+          "mixin",
+          "extend",
+          "content"
         ]
-    }
+      }
+    ],
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        "ignorePseudoClasses": [
+          "global"
+        ]
+      }
+    ]
+  }
 }
